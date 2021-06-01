b71ec258d3bbd790f2e9c19277a2b8c0608f20a0
AmirPaneo <84989189+AmirPaneo@users.noreply.github.com>
2021-06-01 14:41:10 +0800

    menghapus fungsi menampilkan data

--- input-ouput.cpp
+++ input-ouput.cpp
@@ -1,21 +1,12 @@
 #include<iostream>
 using namespace std;
 
-void menampilkanData(string kata, int angka)
-{
-    cout<<"OUTPUTNYA ";cin>>kata;
-    cout<<"Data pertama : "<<kata<<endl;
-    cout<<"Data kedua : "<<angka<<endl;
-}
-
 int main()
 {
     string kata;
     int angka;
-    cout<<"PROGRAM INPUT";cin>>kata;
     cout<<"Masukkan kata  : ";cin>>kata;
     cout<<"Masukkan angka : ";cin>>angka;
-    
-    menampilkanData(kata,angka);
+    cout<<"Data pertama : "<<kata<<endl;
+    cout<<"Data kedua   : "<<angka<<endl;
 }
-
