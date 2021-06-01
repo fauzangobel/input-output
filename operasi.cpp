55cc725dd64163872bc276d18be22b4615aaefa8
fauzangobel <85114050+fauzangobel@users.noreply.github.com>
2021-06-01 14:02:26 +0800

    membuat file operasi.cpp dan inputannya

---
+++ operasi.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int angka1,angka2;
+
+    cout<<"Masukkan Angka Pertama  : ";cin>>angka1;
+    cout<<"Masukkan Angka Kedua    : ";cin>>angka2;
+}
+
+
